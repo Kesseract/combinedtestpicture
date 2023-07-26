13482fa18f2d75bbd64fd556ddc3d143f1cdfed4
Shikigami423 <hikari.i.acg@gmail.com>
2023-07-26 17:40:28 +0900

    画像の範囲をconfigで設定できるように

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 import os
 import pyautogui
+import configparser
 
-base_dir = "../data/combinedtest"  # 保存先ディレクトリを指定
+# 現在のスクリプトのあるディレクトリを取得
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 設定ファイルのパスを指定
+config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../config.ini')
+
+# configparserの設定
+config = configparser.ConfigParser()
+config.read(config_path)
+
+# スクリーンショットの範囲を設定ファイルから読み込む
+left = config.getint('DEFAULT', 'Left')
+top = config.getint('DEFAULT', 'Top')
+width = config.getint('DEFAULT', 'Width')
+height = config.getint('DEFAULT', 'Height')
+
+base_dir = "../data"  # 保存先ディレクトリを指定
 
 # ディレクトリが存在しない場合、作成
 if not os.path.exists(base_dir):
@@ -11,7 +28,7 @@ test_case_dir = base_dir
 
 
 def print_dir_contents(directory):
-    print("Current directories:")
+    print("テストケース一覧:")
     for name in os.listdir(directory):
         if os.path.isdir(os.path.join(directory, name)):
             print(name)
@@ -19,7 +36,7 @@ def print_dir_contents(directory):
 
 while True:
     print_dir_contents(base_dir)
-    print("Please enter the test case directory name (or 'q' to quit):")
+    print("テストケース名を入力してください。 ('q'キーで終了):")
     dir_name = input()
 
     if dir_name == 'q':
@@ -32,7 +49,7 @@ while True:
             os.makedirs(test_case_dir)
 
     while True:
-        print("Please enter the filename number (or ' ' to change directory or 'q' to quit):")
+        print("保存する画像名を入力してください。 (スペースキーでフォルダ変更、'q'キーで終了):")
         filename_num = input()
 
         if filename_num == 'q':
@@ -41,7 +58,7 @@ while True:
             break
 
         # 画面全体のスクリーンショットを取得
-        screenshot = pyautogui.screenshot(region=(0, 110, 1920, 970))
+        screenshot = pyautogui.screenshot(region=(left, top, width, height))
         # 入力された番号でファイル名を作成
         filename = f"{filename_num}.png"
         # ファイルを保存
